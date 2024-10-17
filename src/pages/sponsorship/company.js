5e8a226ef5af9b3514c7f4de7e8ef0f51ad6fd75
Zac Finkelstein <zacfinky@icloud.com>
2024-10-17 17:02:49 -0400

    Added inde file for sponsor logos.

--- src/pages/sponsorship/company.js
+++ src/pages/sponsorship/company.js
@@ -1,8 +1,4 @@
-import COMPSA from "../../../public/sponsors/COMPSA.png";
-import DDQIC from "../../../public/sponsors/DDQIC.png";
-import GITHUB from "../../../public/sponsors/Github.png";
-import REDBULL from "../../../public/sponsors/Redbull.png";
-import QUEENS from "../../../public/sponsors/Queens.png";
+import { QUEENS, REDBULL, GITHUB, DDQIC, COMPSA } from "@/../public/sponsors";
 
 export const company = [
   {
