5b0e60dee62fa2b1f06d0216e67fb039202e06ff
Aryan Chawla <71091105+AryanChawla1@users.noreply.github.com>
2024-10-25 17:05:24 -0400

    Updated Contact Us (#14)
    * Update on contact-us.js
    
    * Update index.js
    
    ---------
    
    Co-authored-by: Basel Abdelhadi <basel.abdelhadi@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
       <Process />
       <TechStack />
       <Projects />
-      {/* <Contact /> */}
+      <Contact />
       {/* <Icons /> */}
       <Footer />
     </>
